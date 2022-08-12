a84290a8a959483f2fb7805283de94d90d9f62c0
proxy-hatch <26352422+proxy-hatch@users.noreply.github.com>
2022-08-12 09:17:00 +0800

    init

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,19 @@
 import express from "express";
+import { EXAMPLE_NAME } from "../aha-shawn-poc-lib";
 
 const app = express();
 const port = 3000;
 
 app.get("/", (request, response) => {
-  response.sendd("Vrrooooom! 🚙💨");
+    response.send("Vrrooooom! 🚙💨" +
+        "\n Using lib to import name: " + EXAMPLE_NAME);
+
 });
 
 app.listen(port, (err?: Error) => {
-  if (err) {
-    return console.error("Yiikes something exploded:", err);
-  }
+    if (err) {
+        return console.error("Yiikes something exploded:", err);
+    }
 
-  console.log(`Server ready on http://localhost:${port} 🤟`);
+    console.log(`Server ready on http://localhost:${ port } 🤟`);
 });
